f42615f171a00f9d42c1b8032903ec8758bc27d7
Luiz André Carvalho <luizandre.ita@gmail.com>
2020-12-17 15:36:00 -0300

    Add Cadastro Funcionario

---
+++ br.com.poo.instituicao-financeira/src/views/funcionario/FormFuncionario.java
@@ -0,0 +1,111 @@
+package views.funcionario;
+
+import views.Botao;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FormFuncionario extends JPanel {
+
+    public FormFuncionario() {
+        setBackground(new Color(220, 220, 220));
+        setLayout(new FlowLayout(FlowLayout.LEFT, 20, 40));
+
+        JTextField campoNome = new JTextField();
+        ;
+        JLabel nome = adicionarLabel("Nome:");
+        campoNome.setPreferredSize(new Dimension(170, 30));
+        add(nome);
+        add(campoNome);
+
+        JTextField campoCpf = new JTextField("111.111.111-11");
+        campoCpf.setPreferredSize(new Dimension(150, 30));
+        JLabel cpf = adicionarLabel("CPF:");
+        add(cpf);
+        add(campoCpf);
+
+        JTextField campoDataNasc = new JTextField("dd-mm-yyyy");
+        campoDataNasc.setPreferredSize(new Dimension(120, 30));
+        JLabel dataNasc = adicionarLabel("Nascimento:");
+        add(dataNasc);
+        add(campoDataNasc);
+
+        JTextField campoSexo = new JTextField();
+        campoSexo.setPreferredSize(new Dimension(150, 30));
+        JLabel sexo = adicionarLabel("Sexo:");
+        add(sexo);
+        add(campoSexo);
+
+        JTextField campoPais = new JTextField();
+        campoPais.setPreferredSize(new Dimension(150, 30));
+        JLabel pais = adicionarLabel("País:");
+        add(pais);
+        add(campoPais);
+
+        JTextField campoUf = new JTextField();
+        campoUf.setPreferredSize(new Dimension(150, 30));
+        JLabel uf = adicionarLabel("Estado:");
+        add(uf);
+        add(campoUf);
+
+        JTextField campoCep = new JTextField();
+        campoCep.setPreferredSize(new Dimension(150, 30));
+        JLabel cep = adicionarLabel("CEP:");
+        add(cep);
+        add(campoCep);
+
+        JTextField campoCidade = new JTextField();
+        campoCidade.setPreferredSize(new Dimension(150, 30));
+        JLabel cidade = adicionarLabel("Cidade:");
+        add(cidade);
+        add(campoCidade);
+
+        JTextField campoRua = new JTextField();
+        campoRua.setPreferredSize(new Dimension(150, 30));
+        JLabel rua = adicionarLabel("Rua:");
+        add(rua);
+        add(campoRua);
+
+        JTextField campoNumero = new JTextField();
+        campoNumero.setPreferredSize(new Dimension(150, 30));
+        JLabel numero = adicionarLabel("Número:");
+        add(numero);
+        add(campoNumero);
+
+        JTextField campoEstadoCiv = new JTextField();
+        campoEstadoCiv.setPreferredSize(new Dimension(90, 30));
+        JLabel civil = adicionarLabel("Estado Civil:");
+        add(civil);
+        add(campoEstadoCiv);
+
+        JTextField campoDataAd = new JTextField("dd-mm-yyyy");
+        campoDataAd.setPreferredSize(new Dimension(110, 30));
+        JLabel dataAd = adicionarLabel("Data Admissão:");
+        add(dataAd);
+        add(campoDataAd);
+
+        JTextField campoCargo = new JTextField();
+        campoCargo.setPreferredSize(new Dimension(160, 30));
+        JLabel cargo = adicionarLabel("Cargo:");
+        add(cargo);
+        add(campoCargo);
+
+        JTextField salario = new JTextField();
+        salario.setPreferredSize(new Dimension(140, 30));
+        JLabel salBase = adicionarLabel("Salário:");
+        add(salBase);
+        add(salario);
+
+        Botao botao = new Botao("Cadatrar");
+        botao.setPreferredSize(new Dimension(140, 40));
+        botao.setBackground(Color.BLUE);
+
+        add(botao);
+    }
+
+    private JLabel adicionarLabel(String labelText) {
+        JLabel label = new JLabel(labelText);
+        label.setFont(new Font("Courier", Font.BOLD, 16));
+        return label;
+    }
+}
--- br.com.poo.instituicao-financeira/src/views/funcionario/PainelFuncionario.java
+++ br.com.poo.instituicao-financeira/src/views/funcionario/PainelFuncionario.java
@@ -1,5 +1,38 @@
 package views.funcionario;
 
-public class PainelFuncionario {
+import java.awt.BorderLayout;
 
+import views.Painel;
+
+public class PainelFuncionario extends javax.swing.JFrame {
+    public PainelFuncionario() {
+        organizarLayout();
+
+        setTitle("Cadastro de Funcionário");
+
+        // Tamanho da tela
+        setSize(550, 720);
+
+        // Aplicação abre no centro da tela
+        setLocationRelativeTo(null);
+
+        // Para finalizar a aplicação
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Fica Visível
+        setVisible(true);
+    }
+
+    void organizarLayout() {
+        Painel painelFuncionario = new Painel("Cadastro de funcionários");
+        add(painelFuncionario, BorderLayout.NORTH);
+
+        FormFuncionario form = new FormFuncionario();
+        add(form);
+
+    }
+
+    public static void main(String[] args) {
+        new PainelFuncionario();
+    }
 }
