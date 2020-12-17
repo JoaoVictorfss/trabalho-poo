c9407c4def6df17f77f73c7a0eb7440e213f26c7
Brunno Viegas <brunnoviegass@gmail.com>
2020-12-17 17:52:19 -0300

    adiciona telaPrincipalAgencia

--- br.com.poo.instituicao-financeira/src/views/agencia/TelaPrincipalAgencia.java
+++ br.com.poo.instituicao-financeira/src/views/agencia/TelaPrincipalAgencia.java
@@ -1,5 +1,42 @@
 package views.agencia;
 
-public class TelaPrincipalAgencia {
+    import java.awt.BorderLayout;
+    import java.awt.Color;
+    
+    import javax.swing.JFrame;
+    
+    import views.Menu;
+    import views.Painel;
+public class TelaPrincipalAgencia extends JFrame {
+    public TelaPrincipalAgencia(){
+        organizarLayout();
+        
+        setTitle("Agencias");
+                
+        //Tamanho da tela
+        setSize(500, 600);
+                
+        //Aplicação abre no centro da tela
+        setLocationRelativeTo(null);
+        
+        //Para finalizar a aplicação
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //Fica Visível
+        setVisible(true);
+    }
 
+    private void organizarLayout() {
+        setLayout(new BorderLayout());
+        
+        Painel painelAgencia = new Painel("Gerenciador de agencias");
+        add(painelAgencia, BorderLayout.NORTH);
+        
+        Menu menu = new Menu();
+        add(menu, BorderLayout.CENTER);
+    }
+    
+    public static void main(String[] args) {
+        new TelaPrincipalAgencia();
+    }
 }
